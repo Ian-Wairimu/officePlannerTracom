3583f734e8f1adc7159e7d9128334116388facf5
wairimu ian <wairimuian@gmail.com>
2021-09-30 11:45:20 +0300

    changed the boardroom bean class

--- src/main/java/ke/co/tracom/officeplanner/boardroom/Boardroom.java
+++ src/main/java/ke/co/tracom/officeplanner/boardroom/Boardroom.java
@@ -1,47 +1,19 @@
 package ke.co.tracom.officeplanner.boardroom;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Boardroom {
     @Id
     private Long boardroomId;
     private String boardroomName;
 
-    public Boardroom() {
-    }
-
-    public Boardroom(Long boardroomId, String boardroomName) {
-        this.boardroomId = boardroomId;
-        this.boardroomName = boardroomName;
-    }
-
-    public Boardroom(String boardroomName) {
-        this.boardroomName = boardroomName;
-    }
-
-    public Long getBoardroomId() {
-        return boardroomId;
-    }
-
-    public void setBoardroomId(Long boardroomId) {
-        this.boardroomId = boardroomId;
-    }
-
-    public String getBoardroomName() {
-        return boardroomName;
-    }
-
-    public void setBoardroomName(String boardroomName) {
-        this.boardroomName = boardroomName;
-    }
-
-    @Override
-    public String toString() {
-        return "Boardroom{" +
-                "boardroomId=" + boardroomId +
-                ", boardroomName='" + boardroomName + '\'' +
-                '}';
-    }
 }
