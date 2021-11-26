1b779f00fbe508c2d8cbe81df0ba6295372391d1
wairimu ian <wairimuian@gmail.com>
2021-11-26 12:18:03 +0300

    deleted all configurtions and pojo to test on a feature

--- src/main/java/ke/co/tracom/officeplanner/repository/BoardroomRepository.java
+++
@@ -1,7 +0,0 @@
-package ke.co.tracom.officeplanner.repository;
-
-import ke.co.tracom.officeplanner.domain.boardroom.Boardroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardroomRepository extends JpaRepository<Boardroom, Long> {
-}
--- src/main/java/ke/co/tracom/officeplanner/repository/OrganizationRepository.java
+++
@@ -1,7 +0,0 @@
-package ke.co.tracom.officeplanner.repository;
-
-import ke.co.tracom.officeplanner.domain.organization.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-}
